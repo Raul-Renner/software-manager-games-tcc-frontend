94581bd19f314ef7382151b658dd6fdc7bf652fa
jonas vera <jonas.vera@foton.la>
2024-05-16 22:02:03 -0300

    feat: criacao de quadro kanban com tabela

--- src/app/pages/pages-home/home-general/home-general.component.ts
+++ src/app/pages/pages-home/home-general/home-general.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class HomeGeneralComponent {
 
+ public belongsToColumn(task: any, columnStatus: any) {
+    return task.status === columnStatus;
+}
+
+  public board = {
+     colunas: [
+      "",
+      "ToDo", 
+      "Preparacao", 
+      "Software",
+      "Arte",
+      "Design",
+      "Som", 
+      "Integracao",
+      "Teste",
+      "Feito" 
+     ],
+ 
+    tagsActivity: [
+    {id: 1, name:'URGENTE', tagsEnum:'URGENT',listTask: [{name: "atividade 1", status: "Feito"},{ name: "atividade 6", status: "ToDo"},{ name: "atividade 2",status: "ToDo"}] },
+    {id: 2, name:'DEPENDENTE', tagsEnum:'DEPENDENT',listTask: [{name: "atividade 3", status: "ToDo"},{ name: "atividade 4 ", status: "Feito"}]},
+    {id: 3, name:'INDEPENDENTE', tagsEnum:'INDEPENDENT', listTask: [{name: "atividade 5", status: "Preparacao"}]},
+    {id: 4, name:'MELHORIA', tagsEnum:'IMPROVEMENT', listTask: [{name: "atividade 8", status: "ToDo"},{ name: "atividade 7", status: "Feito"}]}
+  ]
+ }
+   
+ 
 }
