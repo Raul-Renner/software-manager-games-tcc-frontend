877ec963cbc1b2a5c1540ed7d1770ce8975f62c6
Raul-Renner <raul.renner.9824@outlook.com>
2024-09-20 23:41:51 -0300

    update card move in board

--- src/app/board/board/board.component.ts
+++ src/app/board/board/board.component.ts
@@ -1,5 +1,9 @@
 import { ToastrService } from 'ngx-toastr';
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -12,166 +16,191 @@ import { ViewColumnsComponent } from 'src/app/forms/modal/view-columns/view-colu
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.sass']
+  styleUrls: ['./board.component.sass'],
 })
-export class BoardComponent  implements OnInit, AfterViewInit {
-
+export class BoardComponent implements OnInit, AfterViewInit {
   public activities: Array<any> = [];
   public activitiesUpdate: Array<any> = [];
   activityUpdate: any;
   public userId: any;
   public projectId: any;
   public project: any;
-  public test:any;
-  public user:any;
+  public test: any;
+  public user: any;
 
   public tagsActivity: Array<any> = [
-    {id: 1, name:'URGENTE', tagsEnum:'URGENT', color: '#FF0000'},
-    {id: 2, name:'DEPENDENTE', tagsEnum:'DEPENDENT', color: '#E38623'},
-    {id: 3, name:'INDEPENDENTE', tagsEnum:'INDEPENDENT', color: '#ccc'},
-    {id: 4, name:'MELHORIA', tagsEnum:'IMPROVEMENT', color: '#2B7AD2'}
+    { id: 1, name: 'URGENTE', tagsEnum: 'URGENT', color: '#FF0000' },
+    { id: 2, name: 'DEPENDENTE', tagsEnum: 'DEPENDENT', color: '#E38623' },
+    { id: 3, name: 'INDEPENDENTE', tagsEnum: 'INDEPENDENT', color: '#ccc' },
+    { id: 4, name: 'MELHORIA', tagsEnum: 'IMPROVEMENT', color: '#2B7AD2' },
   ];
 
   public sectorActivity: Array<any> = [];
 
-  constructor(private activityService:ActivityService,
-    private modalService: NgbModal,private elemento: ElementRef,
+  constructor(
+    private activityService: ActivityService,
+    private modalService: NgbModal,
+    private elemento: ElementRef,
     private route: ActivatedRoute,
     private projectService: ProjectService,
     private toast: ToastrService
-    ){}
-  ngAfterViewInit(): void {
-  }
+  ) {}
+  ngAfterViewInit(): void {}
 
   ngOnInit(): void {
-    const userStorage = localStorage.getItem("currentUser") || null;
+    const userStorage = localStorage.getItem('currentUser') || null;
     const currentUser = JSON.parse(userStorage!);
     this.user = currentUser;
     this.projectId = this.route.snapshot.url[2].path;
     this.userId = this.route.snapshot.url[4].path;
-    this.elemento.nativeElement.ownerDocument.body.style.backgroundColor = '#FFFFFF';
+    this.elemento.nativeElement.ownerDocument.body.style.backgroundColor =
+      '#FFFFFF';
     //this.getActivities();
     this.getProject();
   }
 
-  drop(event: CdkDragDrop<any[]>,  rowId: number, columnId: number) {
+  drop(event: CdkDragDrop<any[]>, rowId: number, columnId: number) {
+    const dataTest = event.item.data;
+
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+
+      this.updateSectorCard(
+        event.container.data[event.currentIndex],
+        rowId,
+        columnId
+      );
     } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
+      );
+      this.updateSectorCard(
+        dataTest,
+        rowId,
+        columnId
       );
     }
-    this.updateSectorCard(event.container.data[event.currentIndex], rowId, columnId);
+    if (rowId && columnId) {
 
+    }
   }
 
   getProject(): void {
     this.sectorActivity = [];
-    this.projectService.findBy({
-      projectIds:[this.projectId],
-      organizationId: this.user.organizationId
-    }).subscribe( resp => {
-      this.project = resp.content;
-      this.activities = resp.content.activities;
-      this.insertColumns(resp.content[0].columnsBoard);
-    })
+    this.projectService
+      .findBy({
+        projectIds: [this.projectId],
+        organizationId: this.user.organizationId,
+      })
+      .subscribe((resp) => {
+        this.project = resp.content;
+        this.activities = resp.content.activities;
+        this.insertColumns(resp.content[0].columnsBoard);
+      });
   }
 
   getActivities(): void {
     this.clearBoardCard();
-    this.activityService.findAllBy({
-      organizationId: this.user.organizationId,
-      projectId: this.projectId
-    }).subscribe(resp =>{
-      this.activities = resp.content;
-    });
+    this.activityService
+      .findAllBy({
+        organizationId: this.user.organizationId,
+        projectId: this.projectId,
+      })
+      .subscribe((resp) => {
+        this.activities = resp.content;
+      });
   }
 
-
   createActivity(): void {
-    const modalResult = this.modalService.open(CreateActivityComponent, {size: 'lg', backdrop: 'static'});
+    const modalResult = this.modalService.open(CreateActivityComponent, {
+      size: 'lg',
+      backdrop: 'static',
+    });
     modalResult.componentInstance.content = this.project[0];
     modalResult.result.then((result) => {
-      if(result){
-          this.toast.success("Atividade cadastrada com sucesso!");
+      if (result) {
+        this.toast.success('Atividade cadastrada com sucesso!');
 
-          this.getProject();
+        this.getProject();
       }
-    })
+    });
   }
 
   createColumm(): void {
     const modalResult = this.modalService.open(CreateColumnComponent);
     modalResult.componentInstance.content = this.project;
     modalResult.result.then((result) => {
-      if(result){
+      if (result) {
         this.sectorActivity = [];
         this.getProject();
       }
-    })
+    });
   }
 
-  onDeleteCard(cardId: number, columnId: number){
+  onDeleteCard(cardId: number, columnId: number) {
     this.sectorActivity = this.sectorActivity.map((column: any) => {
-      if(column.id === columnId){
-        column.activities = column.activities.filter((card: any) => card.id !== cardId);
+      if (column.id === columnId) {
+        column.activities = column.activities.filter(
+          (card: any) => card.id !== cardId
+        );
       }
       return column;
     });
     this.activityService.deleteActivity(cardId).subscribe({
       next: (response: any) => {
-        this.toast.success("atividade removida com sucesso!");
+        this.toast.success('atividade removida com sucesso!');
         this.getProject();
       },
       error: (response: any) => {
-        this.toast.error("Erro ao atualizar a atividade de id: " + cardId);
-
-      }
+        this.toast.error('Erro ao atualizar a atividade de id: ' + cardId);
+      },
     });
   }
 
-  clearBoardCard(){
-    this.sectorActivity.forEach(sectorActivity =>{
+  clearBoardCard() {
+    this.sectorActivity.forEach((sectorActivity) => {
       sectorActivity.list = [];
-    })
+    });
   }
 
-  onEditCard(cardId: number, columnId: number){
+  onEditCard(cardId: number, columnId: number) {
     this.getProject();
   }
 
-  updateSectorCard(activity: any, rowId: number, columnId: number){
-
-    activity.activityDependentList.map((element:any) => {
+  updateSectorCard(activity: any, rowId: number, columnId: number) {
+    activity.activityDependentList.map((element: any) => {
       this.activitiesUpdate.push({
         ...element,
-        activityBranch: activity
-
-      })
+        activityBranch: activity,
+      });
     });
-    let tag = this.tagsActivity.find(element => element.id === rowId);
+    let tag = this.tagsActivity.find((element) => element.id === rowId);
 
-    let sector = this.sectorActivity.find(element => element.id === columnId);
-    this.activityUpdate = ({
+    let sector = this.sectorActivity.find((element) => element.id === columnId);
+    this.activityUpdate = {
       columnBoardId: columnId,
       projectId: this.projectId,
       activityDependentIds: this.activitiesUpdate,
-​      description: activity.description,
-​      estimatedTime: activity.estimatedTime,
+      description: activity.description,
+      estimatedTime: activity.estimatedTime,
       usedTime: activity.usedTime,
-​      id: activity.id,
+      id: activity.id,
       identifier: activity.identifier,
-​      isBlock: activity.isBlock,
+      isBlock: activity.isBlock,
       isFinished: activity.isFinished,
-​      sectorActivity: sector.sectorActivity,
-​      statusPriorityEnum: activity.statusPriorityEnum,
-​      tagsEnum: tag.tagsEnum,
-      title: activity.title
-  });
+      sectorActivity: sector.sectorActivity,
+      statusPriorityEnum: activity.statusPriorityEnum,
+      tagsEnum: tag.tagsEnum,
+      title: activity.title,
+    };
     this.activityService.updateSectorActivity(this.activityUpdate).subscribe({
       next: (response) => {
         // this.toast.success("atividade atualizada com sucesso!");
@@ -179,49 +208,51 @@ export class BoardComponent  implements OnInit, AfterViewInit {
         this.activitiesUpdate = [];
       },
       error: (response) => {
-        this.toast.error("Erro ao atualizar os dados da atividade:" + this.activityUpdate.id);
+        this.toast.error(
+          'Erro ao atualizar os dados da atividade:' + this.activityUpdate.id
+        );
         this.activitiesUpdate = [];
-      }
+      },
     });
-
   }
 
-  updateCard(cardId: number, columnId: number){
+  updateCard(cardId: number, columnId: number) {
     this.sectorActivity = [];
     this.getProject();
   }
-  viewActivity(cardId: number, columnId: number){
+  viewActivity(cardId: number, columnId: number) {
     this.sectorActivity = [];
     this.getProject();
   }
 
-  viewColumns(){
-    const modalResult = this.modalService.open(ViewColumnsComponent, {size: 'lg', backdrop: 'static'});
+  viewColumns() {
+    const modalResult = this.modalService.open(ViewColumnsComponent, {
+      size: 'lg',
+      backdrop: 'static',
+    });
     modalResult.componentInstance.content = this.project;
     modalResult.result.then((result) => {
-      if(result){
+      if (result) {
         this.getProject();
       }
     });
   }
 
   insertColumns(columns: any) {
-    columns.forEach((column:any) => {
-      if(column.phase === 'INITIAL'){
+    columns.forEach((column: any) => {
+      if (column.phase === 'INITIAL') {
         this.sectorActivity.push(column);
       }
     });
-    columns.forEach((column:any) => {
-      if(column.phase === 'EXECUTION'){
+    columns.forEach((column: any) => {
+      if (column.phase === 'EXECUTION') {
         this.sectorActivity.push(column);
       }
     });
-    columns.forEach((column:any) => {
-      if(column.phase === 'FINALIZATION'){
+    columns.forEach((column: any) => {
+      if (column.phase === 'FINALIZATION') {
         this.sectorActivity.push(column);
       }
     });
   }
-
-
 }
